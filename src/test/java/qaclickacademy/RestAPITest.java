6c6ab43edbc74c55657fd84057cc6fc3d4aab4ca
sauravbhatia07 <sauravbhatia07@ymail.com>
2021-07-31 23:30:06 +0530

    commit from SAURAV

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -8,6 +8,8 @@ public class RestAPITest {
 	public void RestAPI()
 	{
 		System.out.println("This is RestAPI test");
+		System.out.println("To test git commands");
+		System.out.println("To test git commands1");
 	}
 	
 }
